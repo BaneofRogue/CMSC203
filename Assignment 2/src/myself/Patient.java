d30efd2fe4eb86f65e32b3ece95087f782dad1ac
William <whkeller.dev@gmail.com>
2025-03-07 13:35:37 -0500

    assignment 2

---
+++ Assignment 2/src/myself/Patient.java
@@ -0,0 +1,158 @@
+/*
+ * Class: CMSC203
+ * Instructor: Ashique Tanveer
+ * Description: 
+ * Due Date: 2/28/25
+ * Platform/compiler: Eclipse IDE for Java Developers - 2024-12
+ * I pledge that I have completed the programming assignment 
+* independently. I have not copied the code from a student or   
+* any source. I have not given my code to any student.
+ * Print your Name here: William Keller
+*/
+
+package myself;
+
+public class Patient {
+	private String firstName, 
+	middleName, 
+	lastName,
+	streetAddress, 
+	city,
+	state,
+	zipCode,
+	phoneNumber,
+	emergencyContactName,
+	emergencyContactPhone;
+	
+	public Patient(){
+		//no arg construct
+	}
+
+	//name constructor
+	public Patient(String first_name, String middle_name, String last_name) {
+		firstName = first_name;
+		middleName = middle_name;
+		lastName = last_name;
+	}
+	
+	//full details constructor
+	public Patient(String first_name, String middle_name, String last_name, String street_address, String city_name, String state_name, String zip_code, String phone_number, String emergency_contact_name, String emergency_contact_phone) {
+		firstName = first_name;
+		middleName = middle_name;
+		lastName = last_name;
+		streetAddress = street_address;
+		city = city_name;
+		state = state_name;
+		zipCode = zip_code;
+		phoneNumber = phone_number;
+		emergencyContactName = emergency_contact_name;
+		emergencyContactPhone = emergency_contact_phone;
+	}
+	
+	//mutators;
+	//first name
+	public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String first_name) {
+        firstName = first_name;
+    }
+    
+    //middle name
+    public String getMiddleName() {
+        return middleName;
+    }
+    public void setMiddleName(String middle_name) {
+        middleName = middle_name;
+    }
+    
+    //last name
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String last_name) {
+        lastName = last_name;
+    }
+    
+    //address
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+    public void setStreetAddress(String street_address) {
+        streetAddress = street_address;
+    }
+    
+    //city
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city_name) {
+        city = city;
+    }
+    
+    //state
+    public String getState() {
+        return state;
+    }
+    public void setState(String state_name) {
+        state = state_name;
+    }
+    
+    //zip
+    public String getZipCode() {
+        return zipCode;
+    }
+    public void setZipCode(String zip_code) {
+        zipCode = zip_code;
+    }
+    
+    //phone
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phone_number) {
+        phoneNumber = phone_number;
+    }
+    
+    //emergence contact name
+    public String getEmergencyContactName() {
+        return emergencyContactName;
+    }
+    public void setEmergencyContactName(String emergency_contact_name) {
+        emergencyContactName = emergency_contact_name;
+    }
+    
+    //emergence phone number
+    public String getEmergencyContactPhone() {
+        return emergencyContactPhone;
+    }
+    public void setEmergencyContactPhone(String emergency_contact_phone) {
+        emergencyContactPhone = emergency_contact_phone;
+    }
+    
+    //concat first, middle, last name separated by space: method "buildFullName".
+    
+    public String buildFullName() {
+    	return firstName + " " + middleName + " " + lastName;
+    }
+    
+    // concat street address, city, state, zipcode separated by space;
+    public String buildAddress() {
+    	return streetAddress + " " + city + " " + state + " " + zipCode;
+    }
+    
+    // emergency contact string builder, just like the previous two.
+    public String buildEmergencyContact() {
+		return emergencyContactName + " " + emergencyContactPhone;
+	}
+    
+    // formatted data of patient
+    public String toString() {
+		return "Patient Information:\n" +
+			   "    Name: " + buildFullName() + "\n" +
+			   "    Address: " + buildAddress() + "\n" +
+			   "    Phone: " + phoneNumber + "\n" +
+			   "    EmergencyContact: " + buildEmergencyContact();
+	}
+    
+}
---
+++ Assignment 2/src/myself/PatientDriverApp.java
@@ -0,0 +1,137 @@
+/*
+ * Class: CMSC203
+ * Instructor: Ashique Tanveer
+ * Description: 
+ * Due Date: 2/28/25
+ * Platform/compiler: Eclipse IDE for Java Developers - 2024-12
+ * I pledge that I have completed the programming assignment 
+* independently. I have not copied the code from a student or   
+* any source. I have not given my code to any student.
+ * Print your Name here: William Keller
+*/
+
+package myself;
+
+import java.util.Scanner;
+
+public class PatientDriverApp {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter Patient Information:");
+		System.out.print("First Name: ");
+		String firstName = input.nextLine();
+
+		System.out.print("Middle Name: ");
+		String middleName = input.nextLine();
+
+		System.out.print("Last Name: ");
+		String lastName = input.nextLine();
+
+		System.out.print("Street Address: ");
+		String streetAddress = input.nextLine();
+
+		System.out.print("City: ");
+		String city = input.nextLine();
+
+		System.out.print("State: ");
+		String state = input.nextLine();
+
+		System.out.print("ZIP Code: ");
+		String zipCode = input.nextLine();
+
+		System.out.print("Phone Number: ");
+		String phoneNumber = input.nextLine();
+
+		System.out.print("Emergency Contact Name: ");
+		String emergencyContactName = input.nextLine();
+
+		System.out.print("Emergency Contact Phone: ");
+		String emergencyContactPhone = input.nextLine();
+
+		Patient patient = new Patient(firstName, middleName, lastName, streetAddress, city, state, zipCode, phoneNumber, emergencyContactName, emergencyContactPhone);
+		
+		System.out.println("\nEnter Procedure Information:");
+		System.out.print("Procedure 1 Name: ");
+		String procedure1Name = input.nextLine();
+
+		System.out.print("Procedure 1 Date: ");
+		String procedure1Date = input.nextLine();
+		
+		System.out.print("Practitioner: ");
+		String practitioner1 = (input.nextLine());
+
+		System.out.print("Charges: ");
+		double charges1 = (input.nextDouble());
+		input.nextLine();
+		
+		// NAME DATE ARGS ONLY CONSTRUCT
+		Procedure procedure1 = new Procedure(procedure1Name, procedure1Date);
+		procedure1.setPractitionerName(practitioner1);
+		procedure1.setProcedureCharges(charges1);
+		///////////////////////////////////////////////////
+
+		System.out.print("Procedure 2 Name: ");
+		String procedure2Name = input.nextLine();
+
+		System.out.print("Procedure 2 Date: ");
+		String procedure2Date = input.nextLine();
+
+		System.out.print("Practitioner: ");
+		String practitioner2 = input.nextLine();
+
+		System.out.print("Charges: ");
+		double charges2 = input.nextDouble();
+		input.nextLine();
+
+		// ALL ARGS CONSTRUCT
+		Procedure procedure2 = new Procedure(procedure2Name, procedure2Date, practitioner2, charges2);
+
+		System.out.print("Procedure 3 Name: ");
+		String procedure3Name = input.nextLine();
+
+		System.out.print("Procedure 3 Date: ");
+		String procedure3Date = input.nextLine();
+
+		System.out.print("Practitioner: ");
+		String practitioner3 = (input.nextLine());
+
+		System.out.print("Charges: ");
+		double charges3 = (input.nextDouble());
+		input.nextLine();
+
+		//NO ARGS CONSTRUCT
+		Procedure procedure3 = new Procedure();
+		
+		procedure3.setProcedureName(procedure3Name);
+		procedure3.setProcedureDate(procedure3Date);
+		procedure3.setPractitionerName(practitioner3);
+		procedure3.setProcedureCharges(charges3);
+
+		displayPatient(patient);
+		displayProcedure(procedure1);
+		displayProcedure(procedure2);
+		displayProcedure(procedure3);
+
+		System.out.printf("\nTotal Charges: $%.2f\n", calculateTotalCharges(procedure1, procedure2, procedure3));
+		input.close();
+		
+		System.out.println("Student Name: William Keller");
+		System.out.println("MC#: M21199343");
+		System.out.println("Due Date: 03/07/25");
+	}
+
+	public static void displayPatient(Patient patient) {
+		System.out.println("\n\n" + patient);
+	}
+
+	public static void displayProcedure(Procedure procedure) {
+		System.out.println(procedure);
+	}
+
+	public static double calculateTotalCharges(Procedure p1, Procedure p2, Procedure p3) {
+		return p1.getProcedureCharges() + p2.getProcedureCharges() + p3.getProcedureCharges();
+	}
+}
+
