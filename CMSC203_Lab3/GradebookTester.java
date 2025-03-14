9739f1be10473318001030c27d7b323d7704977e
William <whkeller.dev@gmail.com>
2025-03-13 23:22:00 -0400

    lab3

---
+++ CMSC203_Lab3/GradebookTester.java
@@ -0,0 +1,52 @@
+package myself;
+
+import junit.framework.TestCase;
+
+public class GradebookTester extends TestCase {
+	
+	GradeBook gradebook1 = new GradeBook(5);
+	GradeBook gradebook2 = new GradeBook(5);
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		gradebook1.addScore(3);
+		gradebook1.addScore(2);
+		
+		gradebook2.addScore(1);
+		gradebook2.addScore(2);
+		gradebook2.addScore(3);
+		gradebook2.addScore(4);
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		
+		gradebook1 = null;
+		gradebook2 = null;
+	}
+
+	public void testAddScore() {
+        assertTrue(gradebook1.addScore(4));  // Should return true if added successfully
+        assertEquals(3.0, gradebook1.getScoreSize());  // scoreSize should be 3
+    }
+
+	public void testSum() {
+        // Test if the sum method returns the correct sum
+        assertEquals(5.0, gradebook1.sum());
+        assertEquals(10.0, gradebook2.sum());
+    }
+
+	public void testMinimum() {
+        // Test if the minimum method returns the correct minimum
+        assertEquals(2.0, gradebook1.minimum());  // min score is 2
+        assertEquals(1.0, gradebook2.minimum());  // min score is 1
+    }
+
+	public void testFinalScore() {
+        // Test if the finalScore method correctly drops the lowest score and sums the rest
+        assertEquals(3.0, gradebook1.finalScore());
+        assertEquals(9.0, gradebook2.finalScore());
+    }
+
+}
